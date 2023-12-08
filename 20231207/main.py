3d43ea84b3b1ee3f88286b1ddf210381ed4aa0a9
Yash Sharma <ys749@cornell.edu>
2023-12-08 02:26:43 -0500

    day 7

---
+++ 20231207/main.py
@@ -0,0 +1,61 @@
+from collections import defaultdict
+
+def map_hand_to_values(hand):
+    # maps 2 to A, 3 to B, so on so that all hands are lexico sorted
+    char_map = {
+        '2': 'a',
+        '3': 'b',
+        '4': 'c',
+        '5': 'd',
+        '6': 'e',
+        '7': 'f',
+        '8': 'g',
+        '9': 'h',
+        'T': 'i',
+        'J': 'j',
+        'Q': 'k',
+        'K': 'l',
+        'A': 'm'
+    }
+    s = ""
+    for c in hand:
+        s += char_map[c]
+    return s
+
+def get_score_of_hand(hand):
+    char_to_count = defaultdict(lambda: 0)
+    for c in hand:
+        char_to_count[c] += 1
+    count_to_freq = defaultdict(lambda: 0)
+    for n in char_to_count.values():
+        count_to_freq[n] += 1
+    if count_to_freq[5]:
+        return 7 # five of a kind
+    if count_to_freq[4]:
+        return 6 # four of a kind
+    if count_to_freq[3] == 1 and count_to_freq[2] == 1:
+        return 5 # full house
+    if count_to_freq[3] == 1:
+        return 4 # three of a kind
+    if count_to_freq[2] == 2:
+        return 3 # two pair
+    if count_to_freq[2] == 1:
+        return 2 # one pair
+    return 1 # high card
+        
+
+if __name__ == "__main__":
+    lines = open('input.txt', 'r').readlines()
+    hands = []
+    bids = []
+    for line in lines:
+        hand, bid = line.strip().split()
+        bids.append(int(bid))
+        hands.append(hand)
+    total = 0
+    hands = [(get_score_of_hand(hand), map_hand_to_values(hand), bid) for hand, bid in zip(hands, bids)]
+    hands.sort()
+    for (i, (_, h, b)) in enumerate(hands):
+        print(h)
+        total += (i + 1) * b
+    print(total)
